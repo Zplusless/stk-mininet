fa92ad2dccd2c1bb0120a423ba6e4c5157eef4b8
zplusless <liyuanzhe351@163.com>
2021-11-29 15:01:23 +0800

    update

--- test/test_modify.py
+++ test/test_modify.py
@@ -1,6 +1,9 @@
 import requests
 
-data = {'node1': 'node_0-0', 'node2': 'node_0-1', 'bw': 1, 'delay': '200ms', 'jitter': '50ms', 'loss':1}
+param = {'node1': 'node_0-0', 'node2': 'node_0-1', 'bw': 1, 'delay': '200ms', 'jitter': '50ms', 'loss':1}
+
+data_list = []
+data_list.append(param)
 
 
-requests.post(r'http://127.0.0.1:8000/modify/', json=[data])
+requests.post(r'http://127.0.0.1:8000/modify/', json=data_list)
